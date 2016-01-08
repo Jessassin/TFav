76c95602a4e0e24820e562cff0fb35486775f827
Jessassin <jessesotheremail@aol.com>
2016-01-07 22:29:20 -0600

    Created necessities

---
+++ authconfig-example.js
@@ -0,0 +1,8 @@
+var authconfig = {};
+
+authconfig.consumer_key = 'asdfasdfasdfasdf';
+authconfig.consumer_secret = 'asdfasdfasdfasdf';
+authconfig.access_token_key = 'asdfasdfasdfasdf';
+authconfig.access_token_secret = 'asdfasdfasdfasdf';
+
+module.exports = authconfig;
